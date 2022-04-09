35c0a5d233f1d0b7b3e034f4b1a2038c5d0d3e82
Vrushali Patil <soundarya.patil@gmail.com>
2022-04-08 20:05:47 -0400

    Add default values to status

--- scripts/webapp/robot_web_client.py
+++ scripts/webapp/robot_web_client.py
@@ -37,19 +37,22 @@ class RobotWebClient(Node):
             Joy, '/joy_teleop/joy', self.sub_auto_mode_cb, qos_profile_sensor_data
         )
 
-        self.sub_exit_status_ = self.create_subscription(
-            Bool, '/in_row_flag', self.sub_exit_status_cb, qos_profile_sensor_data
+        self.sub_in_row_status_ = self.create_subscription(
+            Bool, '/in_row_flag', self.sub_in_row_status_cb, qos_profile_sensor_data
         )
         
         timer_period_ = 1.0
         self.timer_ = self.create_timer(timer_period_, self.timer_callback)
 
         self.items = {
-            'gps': {},
-            'speed': {},
-            'battery': {},
-            'row_exit_status': {},
-            'autonomous_mode': {}
+            'gps': {'latitude': 0,
+            'longitude': 0,
+            'latitude': 0},
+            'speed': {'linear': 0,
+            'angular': 0},
+            'battery': 0,
+            'in_row_status': False,
+            'autonomous_mode': True
         }
 
     def sub_gps_fix_cb(self, msg):
@@ -71,8 +74,8 @@ class RobotWebClient(Node):
                 batt_percentage = ( (float(vals.value) - 20) / (28.8 - 20) ) * (100 - 0) + 6
                 self.items['battery'] = batt_percentage
 
-    def sub_exit_status_cb(self,msg):
-        self.items['row_exit_status'] = msg.data
+    def sub_in_row_status_cb(self,msg):
+        self.items['in_row_status'] = msg.data
 
     def sub_auto_mode_cb(self,msg):
         if ((msg.buttons[4] == 1) or (msg.buttons[5] == 1)):
